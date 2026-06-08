de8085eaa3d44d74bbd21f33b62be1a71a6ec05b
Armonte <3513008+Armonte@users.noreply.github.com>
2026-06-08 18:24:22 -0400

    version: bump to 0.2.61 for dev promotion

--- version_rc.h
+++ version_rc.h
@@ -8,6 +8,6 @@
 
 #define FM2K_VER_MAJOR  0
 #define FM2K_VER_MINOR  2
-#define FM2K_VER_PATCH  60
+#define FM2K_VER_PATCH  61
 #define FM2K_VER_BUILD  0
-#define FM2K_VER_STRING "0.2.60"
+#define FM2K_VER_STRING "0.2.61"
